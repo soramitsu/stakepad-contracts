1adb56fd65e3a1b1b82642c04d7f5d257b800576
Ayooluwa Akindeko <akindekoayooluwa@gmail.com>
2024-04-09 21:11:08 +0300

    extra changes to pull request review

--- contracts/ERC20StakingPool.sol
+++ contracts/ERC20StakingPool.sol
@@ -8,14 +8,23 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
+contract  ERC20StakingPool is ReentrancyGuard, Ownable(msg.sender){
 
     using SafeERC20 for IERC20;
     error InsufficientAmount(uint256 amount);
     error TokensInLockup(uint256 currentTime, uint256 unlockTime);
+    modifier onlyAdmin() {
+        require(msg.sender == pool.adminAddress, "Not admin");
+        _;
+    }
+    modifier validPoolPeriod() {
+    require(block.timestamp > pool.poolStartTime, "Pool has not started");
+    require(block.timestamp < pool.poolEndTime, "Pool has ended");
+    _;
+    }
+
 
   
-
     struct User{
         uint256 amount;
         uint256 claimed;
@@ -48,24 +57,18 @@ contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
     event UpdatePool(uint256 totalStaked, uint256 accumulatedRewardTokenPerShare, uint256 lastBlockNumber);
     
 
-    constructor(address _stakeToken, address _rewardToken, uint256 _rewardTokenPerBlock, uint256 _poolStartTime, uint256 _poolEndTime, address _adminAddress){
+    constructor(address _stakeToken, address _rewardToken, uint256 _rewardTokenPerBlock, uint256 _poolStartTime, uint256 _poolEndTime, uint256 _lockupPeriod, address _adminAddress){
         pool.stakeToken = IERC20(_stakeToken);
         pool.rewardToken = IERC20(_rewardToken);
         pool.rewardTokenPerBlock = _rewardTokenPerBlock;
         pool.lastAccessedBlock = block.timestamp;
         pool.poolStartTime = _poolStartTime;
         pool.poolEndTime = _poolEndTime;
+        pool.lockupPeriod = _lockupPeriod;
         pool.adminAddress = _adminAddress;
        pool.isActive = false;
         //pool.lockupPeriod = pool._lockUpPeriod;
     }
-
-    function collectCreationFee (uint256 amount) internal {
-        uint256 percent = (amount * 3) / 100;
-        pool.stakeToken.transferFrom(msg.sender, pool.adminAddress, percent);
-        amount = amount - percent;
-    }
-
   
 
     function updatePool() internal {
@@ -80,25 +83,19 @@ contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
         }
     }
 
-    function viewTokens(address user_address) public view returns(uint256 pendingReward)  { 
+    function viewPendingReward(address userAddress) public view returns(uint256 pendingReward)  { 
         if(block.timestamp > pool.lastAccessedBlock && pool.totalStaked > 0){
             uint256 blockDifference = block.timestamp - pool.lastAccessedBlock;
             uint256 totalNewReward = pool.rewardTokenPerBlock * blockDifference;
             uint256 accummulateRewardTokenPerShare = totalNewReward/pool.totalStaked;
-            User storage user = pool.userInfo[user_address];
+            User storage user = pool.userInfo[userAddress];
             pendingReward = (user.amount * accummulateRewardTokenPerShare) - user.rewardDebt ;          
         } 
         return pendingReward;
     }
 
-  function setUserPendingRewards() internal returns (uint256){
-        User storage user = pool.userInfo[msg.sender];
-        uint256 pendingReward = (user.amount * pool.accumulatedRewardTokenPerShare) - user.rewardDebt ;
-        user.unclaimed += pendingReward;
-        user.rewardDebt = user.amount * pool.accumulatedRewardTokenPerShare;
-        return pendingReward;
-    }
-    function stake(uint256 amount) external {
+    function stake(uint256 amount) external validPoolPeriod{
+        updatePool();
         User storage user = pool.userInfo[msg.sender];
         user.unclaimed += (user.amount * pool.accumulatedRewardTokenPerShare) - user.rewardDebt ;
         user.rewardDebt = user.amount * pool.accumulatedRewardTokenPerShare;
@@ -107,14 +104,12 @@ contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
         pool.stakeToken.safeTransferFrom(msg.sender, address(this) , amount);
         user.stakeTimestamp = block.timestamp;
         emit Stake(msg.sender, amount);
-
-        updatePool();
     }
 
-    function unstake(uint256 amount) external nonReentrant{
-        
+    function unstake(uint256 amount) external nonReentrant validPoolPeriod{
+        updatePool();
         User storage user = pool.userInfo[msg.sender];
-        if(block.timestamp >= user.stakeTimestamp + pool.lockupPeriod)
+        if(block.timestamp < user.stakeTimestamp + pool.lockupPeriod)
             revert TokensInLockup(block.timestamp, user.stakeTimestamp + pool.lockupPeriod);
         if (user.amount > amount){
             user.unclaimed += (user.amount * pool.accumulatedRewardTokenPerShare) - user.rewardDebt ;
@@ -125,17 +120,15 @@ contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
 
             emit Unstake(msg.sender,  amount);
 
-            updatePool();
+            
         }
         else{revert InsufficientAmount(user.amount);}
         
     }
 
-    function claim() external nonReentrant{
+    function claim() external nonReentrant {
+        updatePool();
         User storage user = pool.userInfo[msg.sender];
-
-        if (user.amount < 100)
-            revert InsufficientAmount(user.amount);
 
         user.unclaimed += (user.amount * pool.accumulatedRewardTokenPerShare) - user.rewardDebt ;
         user.rewardDebt = user.amount * pool.accumulatedRewardTokenPerShare;
@@ -144,10 +137,10 @@ contract  ERC20StakingPool is ReentrancyGuard, Ownable (msg.sender){
         pool.rewardToken.safeTransfer(msg.sender, user.unclaimed);
         emit Claim(msg.sender, user.unclaimed);
         user.unclaimed = 0;
-        updatePool();
     }
 
-    function activate() public  {
+    
+    function activate() onlyAdmin external  {
         pool.isActive = true;
         emit ActivatePool();
     }
--- contracts/StakingFactory.sol
+++ contracts/StakingFactory.sol
@@ -5,25 +5,32 @@ SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.25;
 import { ERC20StakingPool } from "./ERC20StakingPool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingFactory {
+contract StakingFactory is Ownable (msg.sender){
+
+    using SafeERC20 for IERC20;
     address[] public stakingPools;
     address adminWalletAddress;
     event CreateStakingPool(address indexed stakingAddress);
-    constructor(address _adminWalletAddress){
-        adminWalletAddress = _adminWalletAddress;
+    constructor(){
+        adminWalletAddress = msg.sender;
     }
 
     function deploy(address _safeToken, address _rewardToken,
-     uint256 _rewardPerBlock, uint256 _poolStartTime, uint256 _poolEndTime) 
+     uint256 _rewardPerBlock, uint256 _poolStartTime, uint256 _poolEndTime, uint256 _lockupPeriod) 
      public returns (address newPoolAddress){
         newPoolAddress = address(
             new ERC20StakingPool{
                 salt: keccak256(abi.encodePacked(_safeToken, _rewardToken,
-                 _rewardPerBlock, _poolStartTime, _poolEndTime))
-            }(_safeToken, _rewardToken, _rewardPerBlock, _poolStartTime, _poolEndTime, adminWalletAddress)
+                 _rewardPerBlock, _poolStartTime, _poolEndTime, _lockupPeriod, adminWalletAddress))
+            }(_safeToken, _rewardToken, _rewardPerBlock, _poolStartTime, _poolEndTime, _lockupPeriod, adminWalletAddress)
         );
         stakingPools.push(newPoolAddress);
         ERC20StakingPool(newPoolAddress).transferOwnership(msg.sender);
+        IERC20(_rewardToken).safeTransfer(newPoolAddress, (_poolEndTime - _poolStartTime) * _rewardPerBlock);
+        emit CreateStakingPool(newPoolAddress);
     }
 }
